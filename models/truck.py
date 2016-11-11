acab978d3be209e1e5e66a42bb63823ce4aeeab5
cesargtz <cesargtz@yecora.mx>
2016-11-11 18:47:55 -0500

    changes cesar

--- models/truck.py
+++ models/truck.py
@@ -7,6 +7,7 @@ class Truck(models.AbstractModel):
 
     driver = fields.Char()
     car_plates = fields.Char()
+    date = fields.Date(default=fields.Date.today)
 
     input_kilos = fields.Float()
     output_kilos = fields.Float()
